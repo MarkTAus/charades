9ec860b5e9b004bb09cf0564a8529a5c3e99ebbb
MarkTAus <78713767+MarkTAus@users.noreply.github.com>
2021-11-30 10:51:49 -0500

    Update charades.py

--- charades.py
+++ charades.py
@@ -5,12 +5,20 @@ print("Directions:")
 print("1. Choose a player to start.")
 print("2. Word will display and player has to act the word out.")
 print("3. The first person to guess the word or phrase gets a point.")
+print("4. Press 1 to start.")
 
-response = requests.get("https://random-word-api.herokuapp.com/word?number=1")
-quoteResponse = requests.get("https://random-word-form.herokuapp.com/random/animal")
+temp = 1
+temp = input("Enter 1 to start: ")
 
-print("The random word is:")
-print(response.text)
+while(temp != 0):
 
-print("Act out this animal:")
-print(quoteResponse)
+  response = requests.get("https://random-word-api.herokuapp.com/word?number=1")
+  quoteResponse = requests.get("https://random-word-form.herokuapp.com/random/animal")
+
+  print("The random word is:")
+  print(response.text)
+
+  print("Act out this animal:")
+  print(quoteResponse)
+  
+  temp = input("Enter 1 to start or 0 to exit.: ")
