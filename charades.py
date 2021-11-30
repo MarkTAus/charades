005f63d8e4e44644181b6396911db47ad1771d0b
Molly Caffery <mollycaffery10@gmail.com>
2021-11-30 10:30:49 -0500

    Initial commit
    Charades

---
+++ charades.py
@@ -0,0 +1,16 @@
+import requests
+
+print("Welcome to Charades!")
+print("Directions:")
+print("1. Choose a player to start.")
+print("2. Word will display and player has to act the word out.")
+print("3. The first person to guess the word or phrase gets a point.")
+
+response = requests.get("https://random-word-api.herokuapp.com/word?number=1")
+quoteResponse = requests.get("https://yusufnb-quotes-v1.p.rapidapi.com/widget/~einstein.json")
+
+print("The random word is:")
+print(response)
+
+print("Guess who said this quote!")
+print(quoteResponse)
